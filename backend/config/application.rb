b1fecf0e6efa0de7773b4ce8725b08673dcabda7
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-11-13 13:21:49 +0100

    use reverse proxy for vue.js, fixes #4

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,9 +11,14 @@ module Yabs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.action_controller.allow_forgery_protection = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
