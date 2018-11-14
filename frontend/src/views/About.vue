cffbf4f80b003df53055553c4d986e9948476e06
Eric Persson <ericpersson@Terrorbyte.local>
2018-11-14 10:21:38 +0100

    Fixed indentation

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,8 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
     <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
-  <p>For more information visit website</p>
-  <b-btn variant="primary" href="#">More Info</b-btn>
-</b-jumbotron>
+      <p>For more information visit website</p>
+      <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
   </div>
 </template>
