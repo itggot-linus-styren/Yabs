e5adb6d22d864dde55e4e4bfdced0f6b11eb9db8
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-11-16 12:45:38 +0100

    Fixed so database is synched every 10 min with json

--- backend/app/jobs/data_fetch_job.rb
+++ backend/app/jobs/data_fetch_job.rb
@@ -6,7 +6,13 @@ class DataFetchJob < ApplicationJob
   def perform(*fetch)
     url = "https://jsonplaceholder.typicode.com/users"
     data = JSON.parse(open(url).read)
-    puts data
+    data.each do |person|
+      if @user = User.find_by(google_token: person["phone"])
+        @user.update(name: person["name"], email: person["email"], google_token: person["phone"])
+      else 
+        User.create(name: person["name"], email: person["email"], google_token: person["phone"])
+      end
+    end
     DataFetchJob.set(wait: 10.minutes).perform_later []
   end
 end
