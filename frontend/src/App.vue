a10f23cfbeb101fc4c414e281ef1191a7cb6b0f2
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-16 10:52:59 +0100

    added google material-icons and added navitems

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
+
 <div>
   <b-navbar type="dark" variant="primary" toggleable>
     <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
     <b-collapse is-nav id="nav_dropdown_collapse">
       <b-navbar-nav>
-        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">Start</b-nav-item>
+        <b-nav-item href="#">Profil</b-nav-item>
+        <b-nav-item href="#">Lägg till</b-nav-item>
+        <b-nav-item href="#">Hitta</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
+    <b-navbar-brand>Yabs</b-navbar-brand>
+    <b-navbar-brand class="material-icons">account_circle</b-navbar-brand>
   </b-navbar>
 </div>
 
@@ -14,8 +20,6 @@
 </template>
 
 <style lang="sass">
-  
-
 </style>
 
 <script lang="ts">
