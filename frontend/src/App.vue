33e8afb88885e93f29a66a4f8157f301ff94d265
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-16 14:22:01 +0100

    made headernav a vue-component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,15 @@
-<template lang="pug">
-
-<div>
-  <b-navbar type="dark" variant="primary" toggleable>
-    <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
-    <b-collapse is-nav id="nav_dropdown_collapse">
-      <b-navbar-nav>
-        <b-nav-item href="#">Start</b-nav-item>
-        <b-nav-item href="#">Profil</b-nav-item>
-        <b-nav-item href="#">Lägg till</b-nav-item>
-        <b-nav-item href="#">Hitta</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-    <b-navbar-brand>Yabs</b-navbar-brand>
-    <b-navbar-brand class="material-icons">account_circle</b-navbar-brand>
-  </b-navbar>
-</div>
-
-
+<template>
+  <HeaderNav/>
 </template>
 
-<style lang="sass">
-</style>
-
 <script lang="ts">
-import Vue from 'vue';
-export default Vue.extend({
-  data: function () {
-    return {
-    }
+import { Component, Vue } from 'vue-property-decorator';
+import HeaderNav from '@/components/HeaderNav.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HeaderNav,
   },
-  methods: {
-  }
-});
+})
+export default class App extends Vue {}
 </script>
