20f8a8b498e9d7c4868c6f87acdb63e55c5fc355
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-20 13:23:10 +0100

    added the two different forms(not done)

---
+++ frontend/src/components/CigForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup3"
+                    label="Cig:"
+                    label-for="exampleInput3">
+        <b-form-select id="exampleInput3"
+                      :options="foods"
+                      required
+                      v-model="form.food">
+        </b-form-select>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CigForm extends Vue {
+    form = {
+        email: '',
+        name: '',
+        food: null,
+        checked: [],
+    };
+    foods = [
+        { text: 'Select One', value: null },
+        'Carrots', 'Beans', 'Tomatoes', 'Corn'
+    ];
+    show = true;
+
+    onSubmit (evt : Event) {
+        evt.preventDefault();
+        alert(JSON.stringify(this.form));
+    }
+
+    onReset (evt : Event) {
+        evt.preventDefault();
+        /* Reset our form values */
+        this.form.email = '';
+        this.form.name = '';
+        this.form.food = null;
+        this.form.checked = [];
+        /* Trick to reset/clear native browser form validation state */
+        this.show = false;
+        this.$nextTick(() => { this.show = true; });
+    }
+}
+</script>
