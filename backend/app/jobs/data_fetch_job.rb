1cf34454112c0f8fd99450b9b27423a075948094
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-11-20 12:52:27 +0100

    fixed so teachers and students are merged

--- backend/app/jobs/data_fetch_job.rb
+++ backend/app/jobs/data_fetch_job.rb
@@ -5,9 +5,10 @@ class DataFetchJob < ApplicationJob
   queue_as :default
 
   def perform(*fetch)
-    has_data = true
     scopes =  ['https://www.googleapis.com/auth/admin.directory.group.readonly','https://www.googleapis.com/auth/admin.directory.user.readonly']
     client = GoogleApiClient.new(Google::Apis::AdminDirectoryV1::DirectoryService, scopes, Rails.application.secrets.google_client_secrets, 'johanneberg.laneservice@ga.ntig.se')
+    data = client.service.list_users(domain: 'ga.ntig.se', query: "orgUnitPath='/Johanneberg'", max_results: 500)
+    insertdata(data.users)
     data = client.service.list_users(domain: 'elev.ga.ntig.se', query: "orgUnitPath='/Johanneberg'", max_results: 500)
     insertdata(data.users)
     while data.next_page_token != nil
@@ -19,11 +20,16 @@ class DataFetchJob < ApplicationJob
 
   def insertdata(users)
      users.each do |person|
-      #Phone is placeholder for Google Token until we plug in real Google Directory Api
+      uid = Time.new.year.to_s[2..-1] + person.id.to_str[13..-1]
       if @user = User.find_by(google_token: person.id)
-        @user.update(name: person.name.full_name, email: person.emails[0]["address"], google_token: person.id)
+        @user.update(name: person.name.full_name, uid: uid, email: person.emails[0]["address"], google_token: person.id)
       else 
-        User.create(name: person.name.full_name, email: person.emails[0]["address"], google_token: person.id)
+        User.create(name: person.name.full_name, uid: uid, email: person.emails[0]["address"], google_token: person.id)
+        if person.org_unit_path == "/Johanneberg/Personal"
+          Role.create(user_id: uid, title: "Lärare")
+        else
+          Role.create(user_id: uid, title: "Elev")
+        end
       end
     end
   end
