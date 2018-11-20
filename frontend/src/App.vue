f6d7be570c81416ac1f56c33d3c3ba30db37b5f1
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-20 13:22:15 +0100

    added base css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,9 +6,24 @@
 </template>
 
 <style lang="sass">
+
+  html, body
+    width: 100vw
+    height: 100vh
+  
   .app
     width: 100vw
     height: 100vh
+
+  .view
+    position: absolute
+    top: 56px
+    bottom: 0
+    width: 100%
+    display: flex
+    flex-direction: row
+
+    
 </style>
 
 <script lang="ts">
