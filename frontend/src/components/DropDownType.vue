126ec490caae577991c929b4b4e0bc6623e9e594
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-20 13:23:38 +0100

    added a dropdown menu to select type of form

---
+++ frontend/src/components/DropDownType.vue
@@ -0,0 +1,35 @@
+<template>
+    <div @click="changeType">
+        <b-dropdown id="ddown1" text="Ändra Typ" class="m-md-2" >
+            <b-dropdown-item @click="changeToLoan">Lån</b-dropdown-item>
+            <b-dropdown-item @click="changeToCig">ID Kort</b-dropdown-item>
+        </b-dropdown>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DropDownType extends Vue {
+
+    @Prop({default: ''})
+    selectedType: string;
+
+    type = this.selectedType;
+
+    changeType () {
+        this.$emit('change-type', this.type)
+    }
+
+    changeToLoan () {
+        this.type = "Lån"
+    }
+
+    changeToCig () {
+        this.type = "ID Kort"
+    }
+
+}
+</script>
