8fb6a053cb9932916fb3222686cb56d70cc0576b
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-20 13:24:30 +0100

    the title change depending on what form you are using

--- frontend/src/views/Add.vue
+++ frontend/src/views/Add.vue
@@ -1,3 +1,61 @@
-<template lang="pug">
-    h1 hje
+<template>
+    <div class="view">
+        <div class="left">
+            <h1>Lägg till {{this.type}}</h1>
+            <DropDownType @change-type="onChangeType($event)" v-bind:selectedType="this.type"/>
+            <LoaningForm v-bind:style="{ display: displayLoan }"/>
+            <CigForm v-bind:style="{ display: displayCig }"/>
+        </div>
+
+        <div class="right">
+        </div>
+    </div>
+
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LoaningForm from '@/components/LoaningForm.vue';
+import CigForm from '@/components/CigForm.vue';
+import DropDownType from '@/components/DropDownType.vue';
+
+@Component({
+  components: {
+    LoaningForm,
+    CigForm,
+    DropDownType,
+  },
+})
+
+export default class Add extends Vue {
+    
+    type = 'Lån';
+
+    displayLoan = 'block'
+    displayCig = 'none'
+
+    onChangeType(type : string) {
+        this.type = type;
+        if(this.type == 'Lån'){
+            this.displayLoan = 'block';
+            this.displayCig = 'none';   
+        } else {
+            this.displayLoan = 'none';
+            this.displayCig = 'block';
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+    .left
+        width: 50%
+        height: 100%
+        padding: 5%
+
+    .right
+        width: 50%
+        height: 100%
+
+    
+</style>
