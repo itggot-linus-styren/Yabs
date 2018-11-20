b108e692f102069cc6db8c450f746cfb46f8a820
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-11-20 10:52:20 +0100

    Fixed so users are synched with database

--- backend/app/jobs/data_fetch_job.rb
+++ backend/app/jobs/data_fetch_job.rb
@@ -1,19 +1,30 @@
-require 'open-uri'
+require 'google/apis/admin_directory_v1'
+require 'googleauth'
 
 class DataFetchJob < ApplicationJob
   queue_as :default
 
   def perform(*fetch)
-    url = "https://jsonplaceholder.typicode.com/users"
-    data = JSON.parse(open(url).read)
-    data.each do |person|
+    has_data = true
+    scopes =  ['https://www.googleapis.com/auth/admin.directory.group.readonly','https://www.googleapis.com/auth/admin.directory.user.readonly']
+    client = GoogleApiClient.new(Google::Apis::AdminDirectoryV1::DirectoryService, scopes, Rails.application.secrets.google_client_secrets, 'johanneberg.laneservice@ga.ntig.se')
+    data = client.service.list_users(domain: 'elev.ga.ntig.se', query: "orgUnitPath='/Johanneberg'", max_results: 500)
+    insertdata(data.users)
+    while data.next_page_token != nil
+      data = client.service.list_users(domain: 'elev.ga.ntig.se', page_token: data.next_page_token, query: "orgUnitPath='/Johanneberg'")
+      insertdata(data.users)
+    end
+    DataFetchJob.set(wait: 24.hours).perform_later []
+  end
+
+  def insertdata(users)
+     users.each do |person|
       #Phone is placeholder for Google Token until we plug in real Google Directory Api
-      if @user = User.find_by(google_token: person["phone"])
-        @user.update(name: person["name"], email: person["email"], google_token: person["phone"])
+      if @user = User.find_by(google_token: person.id)
+        @user.update(name: person.name.full_name, email: person.emails[0]["address"], google_token: person.id)
       else 
-        User.create(name: person["name"], email: person["email"], google_token: person["phone"])
+        User.create(name: person.name.full_name, email: person.emails[0]["address"], google_token: person.id)
       end
     end
-    DataFetchJob.set(wait: 10.minutes).perform_later []
   end
 end
