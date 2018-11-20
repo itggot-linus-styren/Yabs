7ec67c04f762dd3c95ca3cb5c2c39e086ef7ecb4
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-11-20 13:01:17 +0100

    migrations for books

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_100040) do
+ActiveRecord::Schema.define(version: 2018_11_20_115002) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "title_id"
+    t.string "barcode"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title_id"], name: "index_books_on_title_id"
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.integer "loaned_by_id"
+    t.integer "lent_by_id"
+    t.integer "book_id"
+    t.date "return_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_loans_on_book_id"
+    t.index ["lent_by_id"], name: "index_loans_on_lent_by_id"
+    t.index ["loaned_by_id"], name: "index_loans_on_loaned_by_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +39,15 @@ ActiveRecord::Schema.define(version: 2018_11_20_100040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_roles_on_user_id"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "name"
+    t.string "isbn"
+    t.integer "cost"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", id: false, force: :cascade do |t|
