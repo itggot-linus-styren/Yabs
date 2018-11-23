4562b4e6baa953e46502e3e94932fa4ab753a405
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-11-23 12:20:48 +0100

    Fixed json for users

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: {users: @users}
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: {user: @user}
+    end
+
+    def update
+        @user = User.update(profile_image)
+    end
 end
