8d8035876ffe2e9b4b1aabde645cc3711a262b1c
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-11-23 13:06:21 +0100

    removed debug information.

--- frontend/src/components/HeaderNav.vue
+++ frontend/src/components/HeaderNav.vue
@@ -12,38 +12,12 @@
         </b-collapse>
         <b-navbar-brand>Yabs</b-navbar-brand>
         <b-navbar-brand class="material-icons">account_circle</b-navbar-brand>
-
-        <b-btn @click="onClickMe">lol</b-btn>
     </b-navbar>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { Getter } from '../decorators';
 
 @Component
-export default class HeaderNav extends Vue {
-
-  @Getter('titles/all') titles : any;
-  @Getter('books/all') books : any;
-  @Getter('loans/all') loans : any;
-  @Getter('users/all') users : any;
-
-  created() {
-    this.$store.dispatch('titles/all');
-    this.$store.dispatch('books/all');
-    this.$store.dispatch('loans/all');
-    this.$store.dispatch('users/all');
-  }
-
-  onClickMe() {
-    this.$store.dispatch('titles/create', {name: "A book about books"});
-    console.log(JSON.parse(JSON.stringify(this.titles)));
-    this.$store.dispatch('books/create', {barcode: "123123"});
-    console.log(JSON.parse(JSON.stringify(this.books)));
-    this.$store.dispatch('loans/create', {return_at: Date.now()});
-    console.log(JSON.parse(JSON.stringify(this.loans)));
-  }
-
-}
+export default class HeaderNav extends Vue {}
 </script>
