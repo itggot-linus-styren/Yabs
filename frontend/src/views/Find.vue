d63955c72ea6619fa202995a5aa4ca4352dc3765
Eric Persson <ericpersson@Terrorbyte.local>
2018-11-28 10:02:29 +0100

    modded Find.vue added input and search button

--- frontend/src/views/Find.vue
+++ frontend/src/views/Find.vue
@@ -1,3 +1,43 @@
 <template lang="pug">
-    h1 Find
+    div(class='main')
+        h1 Search
+        input(class='search' text)
+        br
+        button(class='bttn' type=submit) Search
+            
+    
 </template>
+
+<style>
+.main{
+    background-color:;
+    width: 50%;
+    height: 50%;
+    border: 1px solid black;
+    margin: auto;
+    text-align: center;
+    font-family:'Times New Roman', Times, serif;
+    border-radius: 30px;
+}
+.bttn{
+    margin-top: 20px;
+    height: 30px;
+    width: 100px;
+    border-radius: 30px;
+    border: 2px solid blue
+
+
+}
+
+.search{
+    margin-top: 20px;
+    
+}
+
+
+
+
+
+
+
+</style>
