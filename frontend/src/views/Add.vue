f3d98eef43f9f38162258181a49f46fb77d43457
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-28 10:53:47 +0100

    pug and mediaquerie

--- frontend/src/views/Add.vue
+++ frontend/src/views/Add.vue
@@ -1,15 +1,14 @@
-<template>
-    <div class="view">
-        <div class="left">
-            <h1>Lägg till {{this.type}}</h1>
-            <DropDownType @change-type="onChangeType($event)" v-bind:selectedType="this.type"/>
-            <LoaningForm v-bind:style="{ display: displayLoan }"/>
-            <CigForm v-bind:style="{ display: displayCig }"/>
-        </div>
-
-        <div class="right">
-        </div>
-    </div>
+<template lang="pug">
+    .view
+        .left
+            .container
+                h1 Lägg till {{this.type}}
+                DropDownType(@change-type='onChangeType($event)', v-bind:selectedType='this.type')
+                LoaningForm(v-bind:style='{ display: displayLoan }')
+                CigForm(v-bind:style='{ display: displayCig }')
+        .right
+            ReacentLoan(v-bind:style='{ display: displayLoan }')
+            CanvasContainer(v-bind:style='{ display: displayCig }')
 
 </template>
 
@@ -18,12 +17,16 @@ import { Component, Vue } from 'vue-property-decorator';
 import LoaningForm from '@/components/LoaningForm.vue';
 import CigForm from '@/components/CigForm.vue';
 import DropDownType from '@/components/DropDownType.vue';
+import ReacentLoan from '@/components/ReacentLoan.vue';
+import CanvasContainer from '@/components/CanvasContainer.vue';
 
 @Component({
   components: {
     LoaningForm,
     CigForm,
     DropDownType,
+    ReacentLoan,
+    CanvasContainer,
   },
 })
 
@@ -41,7 +44,7 @@ export default class Add extends Vue {
             this.displayCig = 'none';   
         } else {
             this.displayLoan = 'none';
-            this.displayCig = 'block';
+            this.displayCig = 'flex';
         }
     }
 }
@@ -51,11 +54,27 @@ export default class Add extends Vue {
     .left
         width: 50%
         height: 100%
-        padding: 5%
+        padding: 10%
+        padding-top: 5%
 
     .right
         width: 50%
         height: 100%
+        padding: 5%
+        padding-left: 0px
 
+    @media only screen and (max-width: 808px)
+        .right, .left
+            width: 100%
+            display: flex
+            flex-direction: row
+            justify-content: center
+            padding: 0px
+
+        .left
+            height: 43%
     
+        .right
+            height: 57%
+
 </style>
