4770ae3ac877030990529b3d4fc96aae45b56096
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-28 10:51:35 +0100

    canvas in right ratio

---
+++ frontend/src/components/CigCanvas.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    .root
+        #canvas
+            //- Put canvas here!
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CigCanvas extends Vue {}
+</script>
+
+<style lang="sass" scoped>
+    .root
+        width: 19vw
+        height: 29.64vw
+        display: flex !important
+        margin-bottom: 1%
+        margin-right: 1%
+        background-color: red
+        justify-self: center
+    
+    #canvas
+        width: 100%
+        height: 100%
+
+    @media only screen and (max-width: 808px)
+        .root
+            width: 38vw
+            height: 59.28vw
+
+</style>
