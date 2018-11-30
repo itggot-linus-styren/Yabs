32f580382d8ae722d8bd20c03e0bcf3d424d6007
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-30 10:59:39 +0100

    added card on right side

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -7,7 +7,8 @@
             CigCanvas
                 // antingen bild eller kortet i canvas
         .right
-            
+            h1 hej
+            .card
 </template>
 
 <script lang="ts">
@@ -25,5 +26,17 @@ export default class App extends Vue {
 </script>
 
 <style lang="sass" scoped>
-
+    .card
+        width: 100%
+        height: 95%
+        overflow-y: auto
+        position: relative
+        min-width: 0
+        background-color: #fff
+        background-clip: border-box
+        border: 1px solid rgba(0, 0, 0, 0.125)
+        border-radius: 0.25rem
+        display: flex
+        flex-direction: row
+        justify-content: center
 </style>
