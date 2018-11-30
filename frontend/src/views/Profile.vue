0603c5c5fa98cc52cd7670b09575eed025ec52ed
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-11-30 10:45:45 +0100

    added basics to profile.vue

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,3 +1,29 @@
 <template lang="pug">
-    h1 Profile
+    .view
+        .left
+            h1 Simon
+            h1 Joahnnesson
+            h1 Elev - TE4
+            CigCanvas
+                // antingen bild eller kortet i canvas
+        .right
+            
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import CigCanvas from '@/components/CigCanvas.vue';
+
+@Component({
+    components:{
+        CigCanvas
+    },
+})
+export default class App extends Vue {
+    
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
