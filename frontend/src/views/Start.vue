dfd6420026b52ebae93e9e415d3299ee87210c3e
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-12-04 17:39:49 +0100

    added router link to start page and some format

--- frontend/src/views/Start.vue
+++ frontend/src/views/Start.vue
@@ -2,17 +2,31 @@
     #wrapper
         #left
         b-container
+            h1 Välkommen till YABS! 
+            br
+            p(class="info") Denna applikation kommer att användas för att underlätta hantering/utlåningen av olika sorters litteratur och utskrivning av Passerkort.
+            br
             b-row
                 b-col
-                    b-button(class="row-btn" :size="size" :variant="primary") Profil
+                    router-link.link.row-btn(to='/users')
+                        b-button.row-btn(:size="size" :variant="primary") Profil
+            p(class="info") På profil sidan så ser du all relevant information agngående din profil tex antal aktiva lån från dig.
+            br
             b-row
                 b-col
-                    b-button(class="row-btn" :size="size" :variant="primary") Hitta
+                    router-link.link.row-btn(to='/add')
+                        b-button.row-btn(:size="size" :variant="primary") Lägg till
+            p(class="info") Lägg till delen används för att skapa nya Passerkort, registera nya lån och lägga till nya böcker.
+            br
             b-row
                 b-col
-                    b-button(class="row-btn" :size="size" :variant="primary") Lägg Till
+                    router-link.link.row-btn(to='/find')
+                        b-button.row-btn(:size="size" :variant="primary") Hitta
+            p(class="info") På hitta delen så kan du söka på allting som denna sida hanterar, Tex Elever, specifika lån eller böcker.
                     
         #right
+            h5 Böcker som ska inlämnas snart:
+            br
             b-table(:sory-by.sync='sortBy', :sort-desc.sync='sortDesc', :items='items', :fields='fields')
 
 </template>
@@ -22,24 +36,33 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class Start extends Vue {
-    sortBy = 'utgångsdatum'
-    sortDesc = false
-    size = "lg"
-    primary = "primary"
-    fields = [
-        {key:'elevnamn', sortable:false},
-        {key:'lärarnamn', sortable:false},
-        {key:'utgångsdatum', sortable:false}
+    public sortBy = 'utgångsdatum';
+    public sortDesc = false;
+    public size = 'lg';
+    public primary = 'primary';
+    public fields = [
+        {key: 'elevnamn', sortable: false},
+        {key: 'lärarnamn', sortable: false},
+        {key: 'utgångsdatum', sortable: false},
     ];
 
-    items = [
-        {elevnamn: "tjena", lärarnamn: "tjenixen", utgångsdatum: "01-01-2019"}
-    ]
+    public items = [
+        {elevnamn: 'tjena', lärarnamn: 'tjenixen', utgångsdatum: '01-01-2019'},
+        {elevnamn: 'tjena', lärarnamn: 'tjenixen', utgångsdatum: '01-01-2019'},
+        {elevnamn: 'tjena', lärarnamn: 'tjenixen', utgångsdatum: '01-01-2019'},
+        {elevnamn: 'tjena', lärarnamn: 'tjenixen', utgångsdatum: '01-01-2019'},
+        {elevnamn: 'tjena', lärarnamn: 'tjenixen', utgångsdatum: '01-01-2019'},
+    ];
 }
 </script>
 
 
 <style>
+
+.info{
+    font-size: 1.2em;
+}
+
 #wrapper{
     display:flex;
     flex-direction: row;
