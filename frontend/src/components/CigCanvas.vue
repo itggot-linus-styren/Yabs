1d7213db2524587212eb22798dbcab988781b64e
itggot-filip-gustavsson <filip.gustavsson@itggot.se>
2018-12-04 12:29:21 +0100

    najs

--- frontend/src/components/CigCanvas.vue
+++ frontend/src/components/CigCanvas.vue
@@ -1,15 +1,74 @@
 <template lang="pug">
     .root
-        #canvas
-            //- Put canvas here!
+        .canvasContainer(ref="")
+            canvas#canvas(ref='canvas')
+            img#barcode(hidden='' ref='barcode')
+            //- button(@click='downloadCanvas') Download
+            img#profile(hidden='' ref='profile' src='@/assets/profil.jpg')
+
 
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import JsBarcode from 'jsbarcode';
+import JSZip from 'jszip';
+import JQuery from 'jquery';
+import FileSaver from 'file-saver';
+import { setTimeout } from 'timers';
 
 @Component
-export default class CigCanvas extends Vue {}
+export default class CigCanvas extends Vue {
+
+    public canvas() {
+        const size = 1;
+        const x: HTMLCanvasElement | any  = this.$refs.canvas;
+        const ctx = x.getContext('2d');
+        const width = x.width;
+        const height = x.height;
+        const img = this.$refs.profile;
+        ctx.drawImage(img, width/2, 0, width/1.5, height/2);
+        console.log(width)
+        console.log(height)
+        ctx.font = '15px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText('Namn Namnsson', width/2, 100);
+        ctx.fillText('Elev', width/2, 200 * size);
+        ctx.font = '10px Arial';
+        ctx.fillText('namn.namnsson@elev.ga.ntig.se', width/2, 240 * size);
+        const element = this.$refs.barcode;
+        JsBarcode(element, '1853563462');
+
+        setTimeout(() => {
+            const brdcode = this.$refs.barcode;
+            ctx.drawImage(brdcode, 25 * size, 250 * size, 150 * size, 50 * size);
+        }, 100);
+    }
+
+    public downloadCanvas() {
+        const zip = new JSZip();
+        const canvas: HTMLCanvasElement | any = this.$refs.canvas;
+        canvas.toBlob(function(blob: any) {
+        zip.file('name.png', blob);
+        zip.generateAsync({type: 'blob'}).then(function(blob) {
+            // window.location = "data:application/zip;base64," + base64;
+            FileSaver.saveAs(blob, 'cards.zip');
+        });
+        });
+
+    }
+
+    mounted() {
+
+        const that = this
+        that.showDiv = true
+        that.$nextTick(function () {
+        that.getWindowWidth()
+        })
+
+        setTimeout(this.canvas, 100);
+    }
+}
 </script>
 
 <style lang="sass" scoped>
@@ -19,12 +78,11 @@ export default class CigCanvas extends Vue {}
         display: flex !important
         margin-bottom: 1%
         margin-right: 1%
-        background-color: red
         justify-self: center
     
-    #canvas
-        width: 100%
-        height: 100%
+    .canvasContainer, #canvas
+        width: inherit
+        height: inherit
 
     @media only screen and (max-width: 808px)
         .root
