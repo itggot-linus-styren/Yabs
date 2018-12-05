c8d59da6b4b75ae29b7122a6de63433db8d4fe1d
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-12-05 13:47:04 +0100

    ja

--- frontend/src/components/CigForm.vue
+++ frontend/src/components/CigForm.vue
@@ -17,7 +17,10 @@ export default class CigForm extends Vue {
     public files: string[] = [];
 
     public submit() {
-        console.log(this.files);
+        
+        
+
+
     }
 
     public onFileSelect(event: any) {
