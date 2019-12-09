67e05e4d24fdd8c479b954b3bafe91550a43ef9f
TE4-arvid-nilber <55836305+TE4-arvid-nilber@users.noreply.github.com>
2019-12-09 13:38:28 +0100

    Feature/139 migrate to rails 6 (#147)
    * Migrated to Rails 6
    
    Signed-off-by: Arvid Nilber <aanilber@gmail.com>
    
    * Added unit testing
    
    Signed-off-by: Arvid Nilber <aanilber@gmail.com>
    
    * Updated API tests
    
    Signed-off-by: Arvid Nilber <aanilber@gmail.com>
    
    * Updated bundler version to tests
    
    Signed-off-by: Arvid Nilber <aanilber@gmail.com>
    
    * Migrated to Rails 6

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,7 +11,6 @@ module Yabs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    #config.active_storage.routes_prefix = '/api/v1'
     config.action_controller.allow_forgery_protection = false
     config.debug_exception_response_format = :default
     #config.api_only = true
@@ -30,9 +29,11 @@ module Yabs
           credentials: true
       end
     end
-
-    # Don't generate system test files.
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.generators.system_tests = nil
+
   end
 end
-
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,13 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-title = Title.new(name: "Programming for dummies", isbn: "420-420", cost: 200)
-title.title_type = "Skönlitteratur"
-title.save
+title = Title.create(name: "Programming for dummies", isbn: "420-420", cost: 200, title_type: "Skönlitteratur")
 
-book = Book.new(barcode: "5000", status: "Broken")
-book.title = title
-book.save
+
+book = Book.create(barcode: "5000", status: "Broken", title: title)
 
 loan = Loan.new(expiration_date: Date.current)
 loan.lent_by = User.find_by_name('Daniel Berg')
@@ -20,11 +17,11 @@ loan.loaned_by = User.find_by_name('Alex Henryz')
 loan.book = book
 loan.save
 
-title.dump_fixture(append=false)
-book.dump_fixture(append=false)
-loan.dump_fixture(append=false)
-loan.lent_by.dump_fixture(append=false)
-loan.loaned_by.dump_fixture(append=true)
+# title.dump_fixture(append=false)
+# book.dump_fixture(append=false)
+# loan.dump_fixture(append=false)
+# loan.lent_by.dump_fixture(append=false)
+# loan.loaned_by.dump_fixture(append=true)
 
 Book.create(barcode: "0001", status: "OK", title: title).dump_fixture
 Book.create(barcode: "0002", status: "OK", title: title).dump_fixture
