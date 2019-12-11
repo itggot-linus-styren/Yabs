8ca84c0acc32ae6c8a8f35d399365ca24eb79905
Jacob Lundberg <jacob.lundberg@elev.ga.ntig.se>
2019-12-11 15:51:48 +0100

    now storing role as integer/bitwise

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,29 @@ class User < ApplicationRecord
   self.primary_key = 'uid'
   has_one_attached :profile_image
 
-  def admin?
-    self[:role] == "Lärare" # || self[:uid] == 1866996084
+  def principal?
+    self.role & 32 == 32
   end
+
+  def admin?
+    # self[:role] == "Lärare" # || self[:uid] == 1866996084
+    self.role & 16 == 16
+  end
+
+  def caretaker?
+    self.role & 8 == 8
+  end
+
+  def teacher?
+    self.role & 4 == 4
+  end
+
+  def eht?
+    self.role & 2 == 2
+  end
+
+  def student?
+    self.role & 1 == 1
+  end
+
 end
