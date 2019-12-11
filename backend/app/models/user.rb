1ba00ea32ca44201d67e4dfd4284db415e1e024b
Jacob Lundberg <jacob.lundberg@elev.ga.ntig.se>
2019-12-11 15:59:03 +0100

    a user with high clearance is now considered to be all beneath aswell

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -8,19 +8,19 @@ class User < ApplicationRecord
 
   def admin?
     # self[:role] == "Lärare" # || self[:uid] == 1866996084
-    self.role & 16 == 16
+    self.role & 16 == 16 || self.role > 16
   end
 
   def caretaker?
-    self.role & 8 == 8
+    self.role & 8 == 8 || self.role > 8
   end
 
   def teacher?
-    self.role & 4 == 4
+    self.role & 4 == 4 || self.role > 4
   end
 
   def eht?
-    self.role & 2 == 2
+    self.role & 2 == 2 || self.role > 2
   end
 
   def student?
