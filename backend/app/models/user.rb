4ae06192c49282df71b305195b029128dc6b18d8
William Bruun <william.bruun@elev.ga.ntig.se>
2019-12-12 13:31:11 +0100

    Reverted old changes since 0 in ruby is true

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,32 +4,32 @@ class User < ApplicationRecord
 
   # Rektor
   def principal?
-    self.role & 16
+    self.role & 32 == 32
   end
 
   # Administratör
   def admin?
-    self.role & 32|| self.role > 32
+    self.role & 16 == 16 || self.role > 16
   end
 
   # Vaktmästare
   def caretaker?
-    self.role & 8 || self.role > 8
+    self.role & 8 == 8 || self.role > 8
   end
 
   # Lärare
   def teacher?
-    self.role & 4|| self.role > 4
+    self.role & 4 == 4 || self.role > 4
   end
 
   # ElevHälsoTeam
   def student_health?
-    self.role & 2 || self.role > 2
+    self.role & 2 == 2 || self.role > 2
   end
 
   # Elev
   def student?
-    self.role & 1
+    self.role & 1 == 1
   end
 
 end
