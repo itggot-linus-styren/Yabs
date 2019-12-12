ffcc8b3c328cdf96d79e4077bdb9db0f7aa0969b
TE4-Daniel-Kull <daniel.kull@elev.ga.ntig.se>
2019-12-12 15:46:21 +0100

    Before merge with develop

--- frontend/src/components/SearchListComponent.vue
+++ frontend/src/components/SearchListComponent.vue
@@ -7,13 +7,13 @@
         label="Search"
         single-line
         hide-details
-      ></v-text-field>
+      />
     </v-card-title>
     <v-data-table 
       :headers="headers"
       :items="items"
       :search="search"
-    ></v-data-table>
+    />
   </v-card>
 </template>
 
