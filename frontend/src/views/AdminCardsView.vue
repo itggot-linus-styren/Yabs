5360966b9f4d0e9ab83ec3c731cb08050ebbfb70
Oskar <oskar@pilborg.se>
2019-12-16 14:23:14 +0100

    Fixed styling for two files

--- frontend/src/views/AdminCardsView.vue
+++ frontend/src/views/AdminCardsView.vue
@@ -5,7 +5,7 @@
       <div style="margin-right: 50px">
         <CardFormComponent @sendImages="onSendImages($event)" />  
       </div>
-      <div>
+      <div v-if="images.length > 0">
         <CanvasContainer :images="images" />
       </div>
     </div>
