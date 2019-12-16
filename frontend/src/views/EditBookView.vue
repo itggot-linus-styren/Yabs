d36b7030afdda4d22ae8a6279c6026c161507a4e
William Bruun <william.bruun@elev.ga.ntig.se>
2019-12-16 15:30:28 +0100

    Template for images added

--- frontend/src/views/EditBookView.vue
+++ frontend/src/views/EditBookView.vue
@@ -1,7 +1,24 @@
 <template>
 <div>
     <!-- Set in image given from google isbn by using book.volumeInfo.imageLinks.small or medium at the end -->
-    <img>
+    <v-img
+        src="https://bad.src/not/valid"
+        lazy-src="https://picsum.photos/id/11/100/60"
+        aspect-ratio="1"
+        class="grey lighten-2"
+        max-width="500"
+        max-height="300"
+      >
+        <template v-slot:placeholder>
+        <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+        >
+            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+    </template>
+    </v-img>
     <BookStatusComponent :title="book.volumeInfo.title" :status="result.status" :barcode="result.barcode" />
 </div>
 </template>
