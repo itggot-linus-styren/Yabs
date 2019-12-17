691028a344515f75c683e548e3a59e6b824a6dec
Jacob Lundberg <jacob.lundberg@elev.ga.ntig.se>
2019-12-17 15:44:52 +0100

    added a helper function to RoleChecker

--- frontend/src/helpers/RoleChecker.ts
+++ frontend/src/helpers/RoleChecker.ts
@@ -1,25 +1,29 @@
-import UsersModule from '../store/modules/UsersModule';
+import UsersModule, { User } from '../store/modules/UsersModule';
 
 export default class RoleChecker {
+
+  private static checkPermission(user: User, required: number): boolean {
+    return user && (user.role & required) == required || (user.role > required);
+  }
   
   static isPrincipal(): boolean {
-    return UsersModule.currentUser && (UsersModule.currentUser.role & 32) == 32 || (UsersModule.currentUser.role > 32);
+    return this.checkPermission(UsersModule.currentUser, 32);
   }
 
   static isAdmin(): boolean {
-    return UsersModule.currentUser && (UsersModule.currentUser.role & 16) == 16 || (UsersModule.currentUser.role > 16);
+    return this.checkPermission(UsersModule.currentUser, 16);
   }
 
   static isCaretaker(): boolean {
-    return UsersModule.currentUser && (UsersModule.currentUser.role & 8) == 8 || (UsersModule.currentUser.role > 8);
+    return this.checkPermission(UsersModule.currentUser, 8);
   }
 
   static isTeacher(): boolean {
-    return UsersModule.currentUser && (UsersModule.currentUser.role & 4) == 4 || (UsersModule.currentUser.role > 4);
+    return this.checkPermission(UsersModule.currentUser, 4);
   }
 
   static isStudentHealth(): boolean {
-    return UsersModule.currentUser && (UsersModule.currentUser.role & 2) == 2 || (UsersModule.currentUser.role > 2);
+    return this.checkPermission(UsersModule.currentUser, 2);
   }
 
   static isStudent(): boolean {
