b7aaa294a7c457838eecdda199a8487e9ff708e0
Mattis Abrahamsson <mattis.abrahamsson@elev.ga.ntig.se>
2019-12-18 14:03:08 +0100

    Unbroke old test

--- frontend/src/helpers/convertArrayToNested.ts
+++ frontend/src/helpers/convertArrayToNested.ts
@@ -1,4 +1,4 @@
-export default function convertList(payload: any[], typeOfId: string): object { //eslint-disable-line @typescript-eslint/no-explicit-any
+export default function convertList(payload: any[], typeOfId: string): any { //eslint-disable-line @typescript-eslint/no-explicit-any
   return payload.reduce((acc, item) => {
     acc[item[typeOfId]] = item;
     return acc;
