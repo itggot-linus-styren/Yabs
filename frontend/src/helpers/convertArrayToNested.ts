1178e3e07e1d77c6e74480f6437cb81d7548599e
Mattis Abrahamsson <mattis.abrahamsson@elev.ga.ntig.se>
2019-12-18 13:48:02 +0100

    Removed unused constants

--- frontend/src/helpers/convertArrayToNested.ts
+++ frontend/src/helpers/convertArrayToNested.ts
@@ -1,7 +1,6 @@
 export default function convertList(payload: any[], typeOfId: string): object { //eslint-disable-line
-  const res = payload.reduce((acc, item) => {
+  return payload.reduce((acc, item) => {
     acc[item[typeOfId]] = item;
     return acc;
   }, {});
-  return res;
 }
--- frontend/src/helpers/convertNestedToArray.ts
+++ frontend/src/helpers/convertNestedToArray.ts
@@ -1,4 +1,3 @@
 export default function convertList(payload: any): any[] { //eslint-disable-line
-  const res = Object.keys(payload).map((id) => payload[id]);
-  return res;
+  return Object.keys(payload).map((id) => payload[id]);
 }
