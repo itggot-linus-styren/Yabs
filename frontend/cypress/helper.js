1b5fa2df06d2eb287ff7e78bb984544c9663f995
André Skv <andre.skvarc@elev.ga.ntig.se>
2020-12-11 11:20:49 +0100

    revmoved all the example test created by cypress!

--- frontend/cypress/helper.js
+++ frontend/cypress/helper.js
@@ -2,5 +2,5 @@ const baseURL = 'localhost'
 
 export default function baseVisit(url = '/') {
     if (Cypress.env('env') == 'test') { cy.visit(`${baseURL}:3000${url}`)}
-    else { cy.visit(`${baseURL}:8081${url}`); } 
+    else { cy.visit(`${baseURL}:8080${url}`); } 
 }
